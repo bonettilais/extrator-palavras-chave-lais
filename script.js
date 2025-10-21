69c12ebb65605a17aa908f4376b4bcecf302e176
bonettilais <lais.bonetti@escola.pr.gov.br>
2025-10-21 09:23:38 -0300

    21/10

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const botaoMostraPalavras = document.querySelectorAll("#botao-palavra");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave(){
+    alert("Fui clicado!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
