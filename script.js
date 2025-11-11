37c915af7df08deaa86d7f5c084cf3e4c00116e8
bonettilais <lais.bonetti@escola.pr.gov.br>
2025-11-11 09:27:32 -0300

    11/11

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {PALAVRAS_RUINS} from "./palavrasRuins";
+
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
@@ -47,7 +49,7 @@ function contaFrequencias(palavras) {
 }
 
 function tiraPalavrasRuins(palavras) {
-    const PALAVRAS_RUINS = new Set(["para", "nós", "das", "como", "que", "eles", "elas", "onde", "porque", "quando", "a", "o"])
+    
     const palavrasBoas = [];
 
     for (let palavra of palavras) {
